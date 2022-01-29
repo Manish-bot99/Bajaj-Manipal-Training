dcc438f02a9837941917abe5bae2221a065a7f83
Manish Panda <manishpanda_ma@srmuniv.edu.in>
2022-01-29 23:14:24 +0530

    jwt

---
+++ Node-API Training/Node_Express_training/Day3/middleware/authorize.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken")
+const User = require("../userModel")
+async function authorize(req, res, next) {
+    try {
+        const authheader = req.headers["authorization"]
+        const token = authheader.replace("Bearer ", "")
+        const decodedtoken = jwt.verify(token, "jamesbond")
+        console.log(decodedtoken)
+        const user = await User.findOne({ email: decodedtoken.email })
+        req.user = user
+        next()
+    } catch (err) {
+        res.send(err)
+    }
+}
+module.exports = authorize
